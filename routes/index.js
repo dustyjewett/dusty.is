c608731bc9749100e506d44074c31ddf261e7be0
Dusty Jewett <dusty.jewett@gmail.com>
2015-12-13 12:14:41 -0800

    Adding gifs

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,26 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Dusty Is' });
 });
 
+var gifOptions = {
+  root: __dirname + '/../gifs/',
+  dotfiles: 'deny',
+  headers: {
+    'x-timestamp': Date.now(),
+    'x-sent': true
+  }
+};
+var sendGif = function(req, res) {
+  var name = req.path.substring(1);
+  if(name.indexOf('.gif') === -1) {
+    name += ".gif";
+  }
+  res.sendFile( name, gifOptions);
+};
+
+router.get('/drinking.coffee(.gif)?', sendGif);
+router.get('/drinking/coffee(.gif)?', sendGif);
+
+router.get('/coding.furiously(.gif)?', sendGif);
+router.get('/coding/furiously(.gif)?', sendGif);
+
 module.exports = router;
