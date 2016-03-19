3ae35c3b6616436d0d4896a9e6020783fc39b47f
Dusty Jewett <dusty.jewett@gmail.com>
2016-03-19 11:00:56 -0700

    Switching to use Env Variables.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Dusty Is
 http://dusty.is/
+
+## Run using pm2
+
+    npm install -g pm2
+
+    pm2 start ecosystem.json
+
+    pm2 deploy production
+    
+    
--- googleAuthClient.js
+++ googleAuthClient.js
@@ -4,4 +4,4 @@ var CLIENT = JSON.parse(fs.readFileSync('client_id.json', 'utf8'));
 var google = require('googleapis');
 var OAuth2 = google.auth.OAuth2;
 
-module.exports = new OAuth2(CLIENT.web.client_id, CLIENT.web.client_secret, CLIENT.web.redirect_uris[1]);
+module.exports = new OAuth2(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, process.env.GOOGLE_REDIRECT_URI);
