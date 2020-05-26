9dc4e1e461bcb9710349a30ab82ae8f7a9162923
Dusty Jewett <dusty.jewett@gmail.com>
2020-05-26 20:35:27 +0200

    serverless!

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,34 @@
 import React from 'react';
-import './app.css';
-import './handheld.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Resume from "./Resume";
+
 
 function App() {
   return (
-    <>
-      <h1>Dusty Is</h1>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <h1>Dusty Is</h1>
 
-      <ul>
-        <li><a href="/a/software.engineer">A Software Engineer</a></li>
-        <li><a href="/a/gamer">A Gamer</a></li>
-        <li><a href="/on/facebook">On Facebook</a></li>
-        <li><a href="/on/twitter">On Twitter</a></li>
-        <li><a href="/on/linkedin">On LinkedIn</a></li>
-        <li><a href="/on/xbox">On Xbox</a></li>
-      </ul>
-    </>
+          <ul>
+            <li><Link to="/a/software.engineer">A Software Engineer</Link></li>
+            <li><Link to="/a/gamer">A Gamer</Link></li>
+            <li><Link to="/on/facebook">On Facebook</Link></li>
+            <li><Link to="/on/twitter">On Twitter</Link></li>
+            <li><Link to="/on/linkedin">On LinkedIn</Link></li>
+            <li><Link to="/on/xbox">On Xbox</Link></li>
+          </ul>
+        </Route>
+        <Route exact path="/a/software.engineer">
+          <Resume />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
